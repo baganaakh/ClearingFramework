f51a331748887e7b6aba841a91ffefba7b0c6bfc
Altanbagana Nasanjargal <baganaakh@yahoo.com>
2020-01-27 12:25:37 +0800

    create awesome fillgrid

--- ClearingFramework/Properties/Settings.Designer.cs
+++ ClearingFramework/Properties/Settings.Designer.cs
@@ -8,22 +8,32 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace ClearingFramework.Properties
-{
-
-
+namespace ClearingFramework.Properties {
+    
+    
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "11.0.0.0")]
-    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase
-    {
-
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.4.0.0")]
+    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
+        
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
-
-        public static Settings Default
-        {
-            get
-            {
+        
+        public static Settings Default {
+            get {
                 return defaultInstance;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=192.168.1.108\\MSX-1003, 1433; Initial Catalog=Clearing; User ID=sa; P" +
+            "assword=Qwerty123456; MultipleActiveResultSets=True; App=EntityFramework&quot;  " +
+            "persist security info=True;Integrated Security=false;")]
+        public string ConnectionString {
+            get {
+                return ((string)(this["ConnectionString"]));
+            }
+            set {
+                this["ConnectionString"] = value;
             }
         }
     }
--- ClearingFramework/pages/Create.xaml.cs
+++ ClearingFramework/pages/Create.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using ClearingFramework;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
@@ -21,18 +23,53 @@ namespace Clearing.pages
         public Create()
         {
             InitializeComponent();
+            FillGrid();
         }
+        #region fill
+        private void FillGrid()
+        {
+            ClearingEntities CE = new ClearingEntities();
+
+            //var data = from d in CE.Accounts select d;
+            //vwOmniAccBalance.ItemsSource = data.ToList();
+            //or
+            var Accountss = CE.Accounts;
+            vwOmniAccBalance.ItemsSource = Accountss.ToList();
+        }
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            FillGrid();
+        }
+        #endregion
+        string connectionString = ClearingFramework.Properties.Settings.Default.ConnectionString;
         #region insert
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            string lname = lName.Text;
-            string fname = fName.Text;
-            string IdNumber = idNumber.Text;
-            string Phone = phone.Text;
-            string Account = account.Text;
-            string SecAcc = secAcc.Text;
-            string email = mail.Text;
+            if (brokCode.SelectedValue == null && stat.SelectedValue == null && linkAc.SelectedValue == null)
+            {
+                MessageBox.Show("Combos are empty Please fill them");
+            }
+            using (ClearingEntities context=new ClearingEntities())
+            {
+                Account acct = new Account
+                {
+                    lname = lName.Text,
+                    fname = fName.Text,
+                    idNum = idNumber.Text,
+                    phone = phonee.Text,
+                    accNum = accountn.Text,
+                    secAcc = secAc.Text,
+                    mail = email.Text,
+                    brokerCode = brokCode.Text,
+                    //state = stat.SelectedValue.ToString(),
+                    linkAcc = linkAc.Text
+                };
+                context.Accounts.Add(acct);
+                context.SaveChanges();
+                //context.Account acc=new 
+            }
         }
         #endregion
+
     }
 }
