2f8d715f20b175c34a028e61584b938796b6af5b
Altanbagana Nasanjargal <baganaakh@yahoo.com>
2020-03-25 14:52:32 +0800

    non_member to ClearingFramework

--- ClearingFramework/Properties/Settings.Designer.cs
+++ ClearingFramework/Properties/Settings.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Non_Member.Properties {
+namespace ClearingFramework.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
