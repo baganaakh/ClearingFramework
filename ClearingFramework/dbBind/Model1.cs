2e2c993581d0094173422f05057dfa2c51c22ca2
Altanbagana Nasanjargal <baganaakh@yahoo.com>
2020-04-10 10:52:42 +0800

    change con string done

--- ClearingFramework/dbBind/Model1.cs
+++ ClearingFramework/dbBind/Model1.cs
@@ -10,8 +10,6 @@ namespace ClearingFramework.dbBind
         public Model1()
             : base("name=Model1")
         {
-            //Database.SetInitializer<Model1>(new CreateDatabaseIfNotExists<Model1>());
-
         }
 
         public virtual DbSet<Account> Accounts { get; set; }
--- ClearingFramework/windows/connect.xaml.cs
+++ ClearingFramework/windows/connect.xaml.cs
@@ -4,6 +4,8 @@ using System.Configuration;
 using System.Data.SqlClient;
 using System.Data.Entity.Core.EntityClient;
 using System.Data.Entity;
+using ClearingFramework.dbBind;
+using System.Linq;
 
 namespace ClearingFramework
 {
@@ -26,79 +28,21 @@ namespace ClearingFramework
             string DBpassword = ssd.Text;
             string databaseName = databases.Text;
             string providerName = "System.Data.SqlClient";
-            SqlConnectionStringBuilder sqlBuilder = new SqlConnectionStringBuilder();
-            sqlBuilder.DataSource = serverName;
-            sqlBuilder.InitialCatalog = databaseName;
-            sqlBuilder.IntegratedSecurity = true;
-
-            string providerString = sqlBuilder.ToString();
-            EntityConnectionStringBuilder entityBuilder = new EntityConnectionStringBuilder();
-            entityBuilder.Provider = providerName;
-            entityBuilder.ProviderConnectionString = providerString;
-            entityBuilder.Metadata = @"res://*/dbBind.Model1.csdl|res://*/dbBind.Model1.ssdl|res://*/dbBind.Model1.msl";
-            MessageBox.Show(entityBuilder.ToString());
-            try
-            {
-                using (EntityConnection conn = new EntityConnection(entityBuilder.ToString()))
-                {
-                    conn.Open();
-                    var connectString = ConfigurationManager.ConnectionStrings["Model1"].ConnectionString;
-                    MessageBox.Show("connected");
-                    //conn.Close();
-                }
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show("fail " + ex.ToString());
-                return;
-            }
-            config.ConnectionStrings.ConnectionStrings["Model1"].ConnectionString = entityBuilder.ToString();
+            string entityBuilder= "Data source="+serverName+";initial catalog="+databaseName+";integrated security=True;MultipleActiveResultSets=True;App=EntityFramework";
+            config.ConnectionStrings.ConnectionStrings["Model1"].ConnectionString = entityBuilder;
             config.Save(ConfigurationSaveMode.Modified);
+            using(Model1 conx=new Model1())
+            {
+                var query = conx.AdminUsers.Where(s => s.uname == "baganaakh").FirstOrDefault<AdminUser>();
+                MessageBox.Show(query.role);
+            }
+
         }
         private void button1_Copy_Click(object sender, RoutedEventArgs e)
         {
             Login log = new Login();
             log.Show();
             this.Close();
-        }
-    }
-    public static class ConnectionTools
-    {
-        public static void ChangeDataBase(this DbContext source,
-                                                string initialCatalog = "",
-                                                string dataSource = "",
-                                                string userId = "",
-                                                string password = "",
-                                                bool integratedSecurity = true,
-                                                string configConnectionStingName = "")
-        {
-            try
-            {
-                var configNameEf = string.IsNullOrEmpty(configConnectionStingName)
-                    ? source.GetType().Name
-                    : configConnectionStingName;
-                var entityCnxStringBuilder = new EntityConnectionStringBuilder
-                    (System.Configuration.ConfigurationManager
-                        .ConnectionStrings[configNameEf].ConnectionString);
-                var sqlCnxStringBuilder = new SqlConnectionStringBuilder
-                    (entityCnxStringBuilder.ProviderConnectionString);
-                if (!string.IsNullOrEmpty(initialCatalog))
-                    sqlCnxStringBuilder.InitialCatalog = initialCatalog;
-                if (!string.IsNullOrEmpty(dataSource))
-                    sqlCnxStringBuilder.DataSource = dataSource;
-                if (!string.IsNullOrEmpty(userId))
-                    sqlCnxStringBuilder.UserID = userId;
-                if (!string.IsNullOrEmpty(password))
-                    sqlCnxStringBuilder.Password = password;
-
-                sqlCnxStringBuilder.IntegratedSecurity = integratedSecurity;
-                source.Database.Connection.ConnectionString
-                    = sqlCnxStringBuilder.ConnectionString;
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.ToString());
-            }
         }
     }
 
